2337f89e032fdf743e712c2468fe5a5092a241b1
Sonal Ranjit <sonal.ranjit3@gmail.com>
2023-11-15 22:10:09 -0500

    Update submissions api (#45)
    * Set default submissions GET to return ordered by latest submissions
    
    * Started breaking up the api methods for submissions

--- bikespace_api/bikespace_api/api/submissions.py
+++ bikespace_api/bikespace_api/api/submissions.py
@@ -4,6 +4,7 @@ from flask import Blueprint, jsonify, request
 from bikespace_api.api.models import Submission, IssueType, ParkingDuration
 from bikespace_api import db
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy import desc
 import json
 from better_profanity import profanity
 
@@ -11,48 +12,11 @@ submissions_blueprint = Blueprint("submissions", __name__)
 
 
 @submissions_blueprint.route("/submissions", methods=["GET", "POST"])
-def get_answers():
+def handle_submissions():
     if request.method == "GET":
-        submissions = Submission.query.all()
-        json_output = []
-
-        for submission in submissions:
-            issues = []
-            for issue in submission.issues:
-                issues.append(issue.value)
-            submission_json = {
-                "id": submission.id,
-                "latitude": submission.latitude,
-                "longitude": submission.longitude,
-                "issues": issues,
-                "parking_duration": submission.parking_duration.value,
-                "parking_time": submission.parking_time,
-                "comments": submission.comments,
-            }
-            json_output.append(submission_json)
-        return jsonify(json_output)
+        return get_submissions(request)
     elif request.method == "POST":
-        json_body = request.json
-        issues = []
-        profanity.load_censor_words()
-        censored_comments = profanity.censor(json_body["comments"])
-        for issue in json_body["issues"]:
-            issues.append(IssueType(issue))
-        try:
-            new_submission = Submission(
-                json_body["latitude"],
-                json_body["longitude"],
-                issues,
-                ParkingDuration(json_body["parking_duration"]),
-                json_body["parking_time"],
-                censored_comments,
-            )
-            db.session.add(new_submission)
-            db.session.commit()
-            return jsonify({"status": "created"}), 201
-        except IntegrityError:
-            db.session.rollback()
-            return jsonify({"status": "Error"})
+        return post_submissions(request)
 
 
 @submissions_blueprint.route("/submissions/<submission_id>", methods=["GET"])
@@ -71,3 +35,48 @@ def get_submission_with_id(submission_id):
         "comments": submission_with_id.comments,
     }
     return jsonify(submission_with_id_json)
+
+
+def get_submissions(request):
+    submissions = Submission.query.order_by(desc(Submission.parking_time)).all()
+    json_output = []
+
+    for submission in submissions:
+        issues = []
+        for issue in submission.issues:
+            issues.append(issue.value)
+        submission_json = {
+            "id": submission.id,
+            "latitude": submission.latitude,
+            "longitude": submission.longitude,
+            "issues": issues,
+            "parking_duration": submission.parking_duration.value,
+            "parking_time": submission.parking_time,
+            "comments": submission.comments,
+        }
+        json_output.append(submission_json)
+    return jsonify(json_output)
+
+
+def post_submissions(request):
+    json_body = request.json
+    issues = []
+    profanity.load_censor_words()
+    censored_comments = profanity.censor(json_body["comments"])
+    for issue in json_body["issues"]:
+        issues.append(IssueType(issue))
+    try:
+        new_submission = Submission(
+            json_body["latitude"],
+            json_body["longitude"],
+            issues,
+            ParkingDuration(json_body["parking_duration"]),
+            json_body["parking_time"],
+            censored_comments,
+        )
+        db.session.add(new_submission)
+        db.session.commit()
+        return jsonify({"status": "created"}), 201
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({"status": "Error"})
