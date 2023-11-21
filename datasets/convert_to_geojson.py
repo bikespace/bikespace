5db385b5693f47c9ecb5c0607bdfb9adac3caaea
Sonal Ranjit <sonal.ranjit3@gmail.com>
2023-11-21 16:04:31 -0500

    Update python geojson converter script and add new geojson data

--- datasets/convert_to_geojson.py
+++ datasets/convert_to_geojson.py
@@ -1,8 +1,11 @@
 from geojson import Feature, FeatureCollection, Point
 import geojson
 import json
+import sys
 
-json_data_file = open("submissions-2018-2023.json")
+json_data_file_path = sys.argv[1]
+json_data_file_name = sys.argv[1].split('.')[0]
+json_data_file = open(json_data_file_path)
 
 json_data = json.load(json_data_file)
 
@@ -25,6 +28,7 @@ for submission in json_data:
 feature_collection = FeatureCollection(features)
 feature_collection.errors()
 geojson_dump = geojson.dumps(feature_collection)
+output_geojson_file_name = json_data_file_name + ".geojson"
 
-with open("submissions-2018-2023.geojson", "w") as output_geojson:
+with open(output_geojson_file_name, "w") as output_geojson:
     output_geojson.write(geojson_dump)
