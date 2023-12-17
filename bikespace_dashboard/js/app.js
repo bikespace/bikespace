1def0f5447f9840fef576183c898192fc27b39b7
Ben Coleman <ben.coleman@alum.utoronto.ca>
2023-12-17 12:54:00 -0500

    Added summary box and issue chart to sidebar (#68)
    * tweaked header height and shadow
    
    Shadow still not showing, may be issue with map div or z-index
    Also changed offset slightly for sidebar shadow
    
    * Added summary box and issue chart to sidebar
    
    Also changed component system to be class-based so they can all inherit the same required methods
    
    Issue chart uses plotly js

---
+++ bikespace_dashboard/js/app.js
@@ -0,0 +1,41 @@
+/**
+ * Shared state class - handles shared data, filtering, and refresh when filters are updated.
+ */
+class SharedState {
+  constructor(data) {
+    this.components = {}
+
+    this.response_data = data.submissions;
+    // Data initially not filtered
+    this.display_data = data.submissions;
+    console.log("display data", this.display_data);
+  }
+
+  refresh() {
+    for(const module of Object.values(this.components)) {
+      module.refresh();
+    }
+  }
+
+  set filters(f) {
+    this.filters = f;
+    this.refresh();
+  }
+}
+
+class Component {
+  /**
+   * Base class for graphs, map, etc. Registers component with shared_state.
+   * @param {string} parent JQuery selector for parent element
+   * @param {string} root_id tag id for root div
+   * @param {Object} shared_state 
+   */
+  constructor(parent, root_id, shared_state) {
+        // register component
+        this.root_key = root_id.replace("-", "_");
+        shared_state.components[this.root_key] = this;
+
+        // add to page
+        $(parent).append(`<div id="${root_id}"></div>`);
+  }
+}
--- bikespace_dashboard/js/map.js
+++ bikespace_dashboard/js/map.js
@@ -1,65 +1,58 @@
-var map = {
-    init: function(shared_state) {
-        // register component
-        shared_state.components.map = this;
+class Map extends Component {
+  constructor(parent, root_id, shared_state) {
+    super(parent, root_id, shared_state);
+    var lmap = L.map('map').setView([43.652771, -79.383756], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(lmap);
+    var markers = L.markerClusterGroup();
 
-        // add to page
-        $('body').append('<div id="map"></div>');
-        var lmap = L.map('map').setView([43.652771, -79.383756], 13);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(lmap);
-        var markers = L.markerClusterGroup();
+    // build popup content
+    const duration_descr = {
+      'minutes': "for <strong>less than an hour</strong>",
+      'hours': "for <strong>several hours</strong>",
+      'overnight': "<strong>overnight</strong>",
+      'multiday': "for <strong>several days</strong>"
+    };
+    const issue_tags = {
+      'not_provided': `<div class="issue issue-not-provided">Bicycle parking was not provided</div>`,
+      'full': `<div class="issue issue-full">Bicycle parking was full</div>`,
+      'damaged': `<div class="issue issue-damaged">Bicycle parking was damaged</div>`,
+      'abandoned': `<div class="issue issue-abandoned">Parked bicycle was abandoned</div>`,
+      'other': `<div class="issue issue-other">Other issue</div>`
+    };
 
-        // build popup content
-        const duration_descr = {
-            'minutes': "for <strong>less than an hour</strong>", 
-            'hours': "for <strong>several hours</strong>",
-            'overnight': "<strong>overnight</strong>", 
-            'multiday': "for <strong>several days</strong>"
-        };
-        const issue_tags = {
-            'not_provided': `<div class="issue issue-not-provided">Bicycle parking was not provided</div>`, 
-            'full': `<div class="issue issue-full">Bicycle parking was full</div>`, 
-            'damaged': `<div class="issue issue-damaged">Bicycle parking was damaged</div>`, 
-            'abandoned': `<div class="issue issue-abandoned">Parked bicycle was abandoned</div>`, 
-            'other': `<div class="issue issue-other">Other issue</div>` 
-        };
+    for (let point of shared_state.display_data) {
+      let issues = point.issues.map((x) => issue_tags[x] ?? `<div class="issue">${x}</div>`).join(" ");
+      let parking_time = new Date(point.parking_time);
+      let parking_time_desc = parking_time.toLocaleString("en-CA", {
+        "dateStyle": "full",
+        "timeStyle": "short"
+      });
+      let comments = `<strong>Comments:</strong> ${point.comments ? point.comments : "<em>none</em>"}`;
 
-        for (let point of shared_state.data.submissions) {
-            let issues = point.issues.map((x) => issue_tags[x] ?? `<div class="issue">${x}</div>`).join(" ");
-            let parking_time = new Date(point.parking_time);
-            let parking_time_desc = parking_time.toLocaleString("en-CA",{"dateStyle": "full", "timeStyle": "short"});
-            let comments = `<strong>Comments:</strong> ${point.comments ? point.comments : "<em>none</em>"}`;
-
-            let content = [
-                `<div class="issue-list"><strong>Issues:</strong> ${issues ? issues : "<em>none</em>"}</div>`,
-                `<p>This person wanted to park ${duration_descr[point.parking_duration] ?? `<strong>${point.parking_duration}</strong>`} on <strong>${parking_time_desc}</strong></p>`,
-                `<p>${comments}</p>`,
-                `<p class="submission-id">ID: ${point.id}</p>`
-            ].join("");
-            var marker = L.marker([point.latitude, point.longitude]);
-            marker.bindPopup(content);
-            markers.addLayer(marker);
-        }
-        lmap.addLayer(markers);
-
-        // improve keyboard navigation
-        $(document).on("keydown", ".marker-cluster", function(e) {
-            if(e.key == "Enter" || e.key == " "){
-                 $(document.activeElement).trigger('click');
-            }
-        })
-        $(document).on("keydown", ".leaflet-marker-icon", function(e) {
-            if(e.key == " "){ // Enter already works
-                 $(document.activeElement).trigger('click');
-            }
-        })
-
-    },
-    refresh: function() {
-        console.log('sidebar refreshing');
+      let content = [
+        `<div class="issue-list"><strong>Issues:</strong> ${issues ? issues : "<em>none</em>"}</div>`,
+        `<p>This person wanted to park ${duration_descr[point.parking_duration] ?? `<strong>${point.parking_duration}</strong>`} on <strong>${parking_time_desc}</strong></p>`,
+        `<p>${comments}</p>`,
+        `<p class="submission-id">ID: ${point.id}</p>`
+      ].join("");
+      var marker = L.marker([point.latitude, point.longitude]);
+      marker.bindPopup(content);
+      markers.addLayer(marker);
     }
-};
+    lmap.addLayer(markers);
 
-
+    // improve keyboard navigation
+    $(document).on("keydown", ".marker-cluster", function(e) {
+      if (e.key == "Enter" || e.key == " ") {
+        $(document.activeElement).trigger('click');
+      }
+    })
+    $(document).on("keydown", ".leaflet-marker-icon", function(e) {
+      if (e.key == " ") { // Enter already works
+        $(document.activeElement).trigger('click');
+      }
+    })
+  }
+}
