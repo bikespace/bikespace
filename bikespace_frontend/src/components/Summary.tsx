b21cf21c10a75257fcbc3d87dffea112505ba498
hulloanson <pm@hulloanson.com>
2024-01-06 22:47:18 -0500

    initiated google typescript style as eslint and prettier config

--- bikespace_frontend/src/components/Summary.tsx
+++ bikespace_frontend/src/components/Summary.tsx
@@ -1,45 +1,66 @@
-import React from "react";
-import Submission from "../interfaces/Submission";
-import { SubmissionStatus } from "../interfaces/Submission";
+import React from 'react';
+import Submission from '../interfaces/Submission';
+import {SubmissionStatus} from '../interfaces/Submission';
 
-const Summary = (props: { submision: Submission, submissionStatus: SubmissionStatus }) => {
+const Summary = (props: {
+  submision: Submission;
+  submissionStatus: SubmissionStatus;
+}) => {
   const submission = props.submision;
   const submissionStatus = props.submissionStatus;
-  if (submissionStatus.status == "summary") {
+  if (submissionStatus.status === 'summary') {
     return (
       <div id="submission-summary">
         <h1>Summary</h1>
         <div>
-          <p><strong>Issues:</strong> {submission.issues.join(', ').toString()}</p>
-          <p><strong>Location:</strong> {submission.location.latitude.toString()}, {submission.location.longitude.toString()} </p>
-          <p><strong>Time:</strong> {submission.parkingTime.date.toDateString()} </p>
-          <p><strong>Parking duration needed:</strong> {submission.parkingTime.parkingDuration}</p>
-          <p><strong>Comments:</strong> {submission.comments}</p>
+          <p>
+            <strong>Issues:</strong> {submission.issues.join(', ').toString()}
+          </p>
+          <p>
+            <strong>Location:</strong> {submission.location.latitude.toString()}
+            , {submission.location.longitude.toString()}{' '}
+          </p>
+          <p>
+            <strong>Time:</strong> {submission.parkingTime.date.toDateString()}{' '}
+          </p>
+          <p>
+            <strong>Parking duration needed:</strong>{' '}
+            {submission.parkingTime.parkingDuration}
+          </p>
+          <p>
+            <strong>Comments:</strong> {submission.comments}
+          </p>
         </div>
       </div>
     );
-  } else if (submissionStatus.status == "success") {
+  } else if (submissionStatus.status === 'success') {
     return (
       <div id="submission-summary">
         <h1>Success</h1>
         <p>Your submission has been entered successfully!</p>
         <p>Thank You!</p>
       </div>
-    )
-  } else if (submissionStatus.status == "error") {
+    );
+  } else if (submissionStatus.status === 'error') {
     return (
       <div id="submission-summary">
         <h1>Oops</h1>
-        <p>Something went wrong on our end processing your submission, please try again later!</p>
+        <p>
+          Something went wrong on our end processing your submission, please try
+          again later!
+        </p>
       </div>
-    )
+    );
   } else {
     return (
       <div id="submission-summary">
         <h1>Oops!</h1>
-        <p>Something went wrong beyond our expectations. Please try again later, and report this bug to the developers :(</p>
+        <p>
+          Something went wrong beyond our expectations. Please try again later,
+          and report this bug to the developers :(
+        </p>
       </div>
-    )
+    );
   }
 };
 
--- bikespace_frontend/src/pages/index.tsx
+++ bikespace_frontend/src/pages/index.tsx
@@ -1,53 +1,51 @@
-import * as React from "react";
-import { graphql, PageProps, navigate, HeadProps } from "gatsby";
-import { button, title, logo, main, mainContent } from "../styles/index.css";
-import { StaticImage } from "gatsby-plugin-image";
+import * as React from 'react';
+import {graphql, PageProps, navigate, HeadProps} from 'gatsby';
+import {button, title, logo, main, mainContent} from '../styles/index.css';
+import {StaticImage} from 'gatsby-plugin-image';
 
 type DataProps = {
-    site: {
-        siteMetadata: {
-            title: string;
-        };
+  site: {
+    siteMetadata: {
+      title: string;
     };
+  };
 };
 
-const IndexRoute = ({ data: { site } }: PageProps<DataProps>) => {
-    return (
-        <main className={main}>
-            <div className={mainContent}>
-                <h1 className={title}>{site.siteMetadata.title}</h1>
-                <StaticImage
-                    className={logo}
-                    src="../images/bikespace-logo.svg"
-                    alt="BikeSpace Logo"
-                />
-                <button
-                    className={button}
-                    onClick={() => {
-                        navigate("/submission");
-                    }}
-                >
-                    Report a parking issue
-                </button>
-            </div>
-        </main>
-    );
+const IndexRoute = ({data: {site}}: PageProps<DataProps>) => {
+  return (
+    <main className={main}>
+      <div className={mainContent}>
+        <h1 className={title}>{site.siteMetadata.title}</h1>
+        <StaticImage
+          className={logo}
+          src="../images/bikespace-logo.svg"
+          alt="BikeSpace Logo"
+        />
+        <button
+          className={button}
+          onClick={() => {
+            navigate('/submission');
+          }}
+        >
+          Report a parking issue
+        </button>
+      </div>
+    </main>
+  );
 };
 
 export default IndexRoute;
 
 export function Head(props: HeadProps<DataProps>) {
-    return (
-        <title>{props.data.site.siteMetadata.title}</title>
-    )
+  return <title>{props.data.site.siteMetadata.title}</title>;
 }
 
 export const query = graphql`
-    {
-        site {
-            siteMetadata {
-                title
-            }
-        }
+  {
+    site {
+      siteMetadata {
+        title
+      }
     }
+  }
 `;
