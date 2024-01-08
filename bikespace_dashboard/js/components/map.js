1bd16c2f50482dd390395a4b673225e3e9d6bbc7
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-01-07 21:57:59 -0500

    added custom markers and shared issue attributes
    - added custom markers to indicate issue type on map
    - added shared module for issue attributes
    - tweaked some of the labels used to describe issues
    - changed color of marker clusters
    - changed colors on issue chart to match shared attributes

--- bikespace_dashboard/js/components/map.js
+++ bikespace_dashboard/js/components/map.js
@@ -1,4 +1,5 @@
 import { Component } from './main.js';
+import { issue_attributes as ia } from './issue_attributes.js';
 
 const tiles = {
   'thunderforest_atlas': "https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=66ccf6226ef54ef38a6b97fe0b0e5d2e",
@@ -40,13 +41,15 @@ class Map extends Component {
       'overnight': "<strong>overnight</strong>",
       'multiday': "for <strong>several days</strong>"
     };
-    const issue_tags = {
-      'not_provided': `<div class="issue issue-not-provided">Bicycle parking was not provided</div>`,
-      'full': `<div class="issue issue-full">Bicycle parking was full</div>`,
-      'damaged': `<div class="issue issue-damaged">Bicycle parking was damaged</div>`,
-      'abandoned': `<div class="issue issue-abandoned">Parked bicycle was abandoned</div>`,
-      'other': `<div class="issue issue-other">Other issue</div>`
-    };
+    let issue_tags = {};
+    for (let entry of Object.values(ia)) {
+      issue_tags[entry.id] = [
+        `<div class="issue issue-${entry.id.replace('_', '-')}" `,
+          `style="border-color:${entry.color};`,
+            `background-color:${entry.color_light};"`,
+        `>${entry.label_long}</div>`
+      ].join("");
+    }
 
     for (let point of this.shared_state.display_data) {
       let issues = point.issues.map((x) => issue_tags[x] ?? `<div class="issue">${x}</div>`).join(" ");
@@ -58,12 +61,40 @@ class Map extends Component {
       let comments = `<strong>Comments:</strong> ${point.comments ? point.comments : "<em>none</em>"}`;
 
       let content = [
-        `<div class="issue-list"><strong>Issues:</strong> ${issues ? issues : "<em>none</em>"}</div>`,
+        `<div class="issue-list">`,
+        `<strong>Issues:</strong> ${issues ? issues : "<em>none</em>"}`,
+        `</div>`,
         `<p>This person wanted to park ${duration_descr[point.parking_duration] ?? `<strong>${point.parking_duration}</strong>`} on <strong>${parking_time_desc}</strong></p>`,
         `<p>${comments}</p>`,
         `<p class="submission-id">ID: ${point.id}</p>`
       ].join("");
-      var marker = L.marker([point.latitude, point.longitude]);
+
+      // build custom markers
+      // @TODO use class inheritance instead like tutorial
+      const marker_defaults = {
+        shadowUrl: "./leaflet/images/marker-shadow.png",
+        iconSize:     [36, 36],
+        iconAnchor:   [18, 36], // half of width and full height
+        popupAnchor:  [0, -30], // nearly all the height, not sure why negative
+        shadowSize:   [41, 41], // from default
+        shadowAnchor: [12, 41], // more manual offset, bottom point of shadow is ~30% along x axis, not at (0, 0)
+      };
+      // @TODO tag list should also go in this order
+      // @TODO make marker color programmatic?
+
+      // pick the relevant issue to display via marker
+      const marker_issue = point.issues.reduce(
+        (a, c) => ia[a]?.render_priority < ia[c]?.render_priority ? a : c,
+        null);
+      const custom_marker = ia[marker_issue ?? "other"];
+
+      let customIcon = L.icon({
+        iconUrl: custom_marker.icon,
+        ...marker_defaults,
+      })
+      // let customIcon = L.divIcon();
+
+      var marker = L.marker([point.latitude, point.longitude], {icon: customIcon});
       marker.bindPopup(content);
       markers.addLayer(marker);
     }
