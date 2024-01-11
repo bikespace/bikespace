40fb98bcde0c246d8cca1f378d1ac1eaf1574fe4
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-01-10 22:49:23 -0500

    Updated indentation

--- bikespace_frontend/src/pages/index.tsx
+++ bikespace_frontend/src/pages/index.tsx
@@ -4,74 +4,74 @@ import { button, title, logo, main, mainContent } from "../styles/index.css";
 import { StaticImage } from "gatsby-plugin-image";
 
 type DataProps = {
-    site: {
-        siteMetadata: {
-            title: string;
-        };
+  site: {
+    siteMetadata: {
+      title: string;
     };
+  };
 };
 
 const IndexRoute = ({ data: { site } }: PageProps<DataProps>) => {
-    return (
-        <main className={main}>
-            <header>
-                <nav className={"main-nav"} aria-label="Main">
-                    <ul>
-                        <li>
-                            <a href="https://bikespace.ca/">About BikeSpace</a>
-                        </li>
-                        <li>
-                            <a href="https://dashboard.bikespace.ca/">Explore the Data</a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/bikespace/bikespace/tree/main/bikespace_frontend">
-                            <StaticImage
-                                height={20}
-                                style={{marginRight: '0.3rem'}}
-                                id="github-logo"
-                                src="../images/github-mark-white.svg"
-                                alt="Github Logo"
-                            />
-                                Contribute
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <div className={mainContent}>
-                <h1 className={title}>{site.siteMetadata.title}</h1>
-                <StaticImage
-                    className={logo}
-                    src="../images/bikespace-logo.svg"
-                    alt="BikeSpace Logo"
+  return (
+    <main className={main}>
+      <header>
+        <nav className={"main-nav"} aria-label="Main">
+          <ul>
+            <li>
+              <a href="https://bikespace.ca/">About BikeSpace</a>
+            </li>
+            <li>
+              <a href="https://dashboard.bikespace.ca/">Explore the Data</a>
+            </li>
+            <li>
+              <a href="https://github.com/bikespace/bikespace/tree/main/bikespace_frontend">
+              <StaticImage
+                height={20}
+                style={{marginRight: '0.3rem'}}
+                id="github-logo"
+                src="../images/github-mark-white.svg"
+                alt="Github Logo"
                 />
-                <button
-                    className={button}
-                    onClick={() => {
-                        navigate("/submission");
-                    }}
-                >
-                    Report a parking issue
-                </button>
-            </div>
-        </main>
-    );
+                Contribute
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className={mainContent}>
+        <h1 className={title}>{site.siteMetadata.title}</h1>
+        <StaticImage
+            className={logo}
+            src="../images/bikespace-logo.svg"
+            alt="BikeSpace Logo"
+        />
+        <button
+            className={button}
+            onClick={() => {
+                navigate("/submission");
+            }}
+        >
+            Report a parking issue
+        </button>
+      </div>
+    </main>
+  );
 };
 
 export default IndexRoute;
 
 export function Head(props: HeadProps<DataProps>) {
-    return (
-        <title>{props.data.site.siteMetadata.title}</title>
-    )
+  return (
+    <title>{props.data.site.siteMetadata.title}</title>
+  )
 }
 
 export const query = graphql`
-    {
-        site {
-            siteMetadata {
-                title
-            }
-        }
+  {
+    site {
+      siteMetadata {
+        title
+      }
     }
+  }
 `;
