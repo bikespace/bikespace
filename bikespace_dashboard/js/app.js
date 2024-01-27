0bdfcf86d306f321440fa8176bf854c191906c98
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-01-27 15:54:45 -0500

    Add Feedback mailtos to app and dashboard (#101)
    * Added mailto feedback link to dashboard
    * Added feedback mailto to app submission page
    * mailto links use encodeURIComponent, for dashboard the link uses progressive enhancement so that the mailto still works with no JS
    * added an umami tracking attribute (on dashboard link; not yet set up for app)

--- bikespace_dashboard/js/app.js
+++ bikespace_dashboard/js/app.js
@@ -5,6 +5,7 @@ import {IssueChart} from './components/sidebar/issue_chart.js';
 import {DayChart} from './components/sidebar/day_chart.js';
 import {DurationTimeOfDayChart} from './components/sidebar/duration_tod_chart.js';
 
+// Load data from BikeSpace API
 $.ajax({
   url: 'https://api-dev.bikespace.ca/api/v2/submissions?limit=5000',
   success: function (data) {
@@ -17,3 +18,27 @@ $.ajax({
     new Map('body', 'map', shared_state);
   },
 });
+
+// Enhance feedback mailto link
+const feedback_to = 'bikespaceto@gmail.com';
+const feedback_subject = 'BikeSpace Dashboard Feedback';
+const feedback_body = `----------
+Please describe your feedback about dashboard.bikespace.ca below. We welcome both positive feedback (e.g. I found x feature useful) and constructive feedback (e.g. y is broken, I wish the dashboard did z).
+
+Especially for constructive feedback, you can help us by letting us know:
+- Your browser and platform (e.g. Safari, iPhone)
+- If it's a bug, what steps led to the problem
+- If it's something you wish the dashboard was able to do, what goal would that feature help you accomplish? (e.g. "I wanted to see only issues along street x", or "I wanted to better understand issues of y type")
+
+Thank you for taking the time to help us make the dashboard better!
+----------
+
+Hi BikeSpace team,
+
+`;
+const new_href = [
+  `mailto:${feedback_to}`,
+  `?subject=${encodeURIComponent(feedback_subject)}`,
+  `&body=${encodeURIComponent(feedback_body)}`,
+].join('');
+document.getElementById('mailto-feedback').href = new_href;
---
+++ bikespace_frontend/src/components/FeedbackMailTo.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {StaticImage} from 'gatsby-plugin-image';
+
+const feedbackTo = 'bikespaceto@gmail.com';
+const feedbackSubject = 'BikeSpace App Feedback';
+const feedbackBody = `----------
+Please describe your feedback about app.bikespace.ca below. We welcome both positive feedback (e.g. I found x feature useful) and constructive feedback (e.g. y is broken, I wish the app did z).
+
+Especially for constructive feedback, you can help us by letting us know:
+
+- Your browser and platform (e.g. Safari, iPhone)
+- If it's a bug, what steps led to the problem
+- If it's something you wish the app was able to do, what goal would that feature help you accomplish? (e.g. "I think it would help advocates if you could report issues of x type", or "I wanted to be able to do step y easier on my phone")
+
+Thank you for taking the time to help us make the app better!
+----------
+
+Hi BikeSpace team,
+
+`;
+const feedbackHref = [
+  `mailto:${feedbackTo}`,
+  `?subject=${encodeURIComponent(feedbackSubject)}`,
+  `&body=${encodeURIComponent(feedbackBody)}`,
+].join('');
+
+const FeedbackMailTo = () => {
+  return (
+    <a href={feedbackHref}>
+      <StaticImage
+        height={20}
+        style={{marginRight: '0.3rem'}}
+        src="../images/envelope-at.svg"
+        alt="Email Icon"
+      />
+      Feedback
+    </a>
+  );
+};
+
+export default FeedbackMailTo;
