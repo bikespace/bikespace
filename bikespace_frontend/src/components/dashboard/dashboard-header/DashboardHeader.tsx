ce3fa2d47a6a7bbc22665ebe489c9bc86b7f1e9b
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-06-05 17:06:39 -0400

    integrate dashboard header

--- bikespace_frontend/src/components/dashboard/dashboard-header/DashboardHeader.tsx
+++ bikespace_frontend/src/components/dashboard/dashboard-header/DashboardHeader.tsx
@@ -1,15 +1,19 @@
 import React, {useState} from 'react';
-
-import {FeedbackLink} from '../feedback-link/FeedbackLink';
+import {Link} from 'gatsby';
+import {useClickAway} from '@uidotdev/usehooks';
 
 import * as styles from './dashboard-header.module.scss';
 
 import hamburgerMenu from '@/assets/icons/hamburger-menu.svg';
 import bikespaceLogo from '@/assets/icons/bikespace_wordmark.png';
 import githubLogo from '@/assets/icons/github-mark.svg';
+import mailtoIcon from '@/assets/icons/envelope-at.svg';
 
 export function DashboardHeader() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const clickAwayRef = useClickAway(() => {
+    setIsMenuOpen(false);
+  }) as React.RefObject<HTMLDivElement>;
 
   return (
     <header className={styles.header}>
@@ -25,18 +29,16 @@ export function DashboardHeader() {
       <nav
         className={`${styles.mainNav} ${isMenuOpen ? styles.open : ''}`}
         aria-label="Main"
+        ref={clickAwayRef}
       >
-        <a href="#" title="Dashboard Home" className={styles.bikespaceLogo}>
+        <Link to="/" title="Dashboard Home" className={styles.bikespaceLogo}>
           <img src={bikespaceLogo} alt="BikeSpace logo" />
-        </a>
+        </Link>
         <ul>
           <li>
-            <a
-              href="https://app.bikespace.ca/submission/"
-              data-umami-event="outbound-app"
-            >
+            <Link to="/submission" data-umami-event="outbound-app">
               Report Bike Parking Issue
-            </a>
+            </Link>
           </li>
           <li>
             <a
@@ -56,18 +58,36 @@ export function DashboardHeader() {
             </a>
           </li>
           <li>
-            <FeedbackLink />
+            <a
+              href={`mailto:${feedbackTo}?subject=${encodeURIComponent(
+                feedbackSubject
+              )}&body=${encodeURIComponent(feedbackBody)}`}
+              data-umami-event="mailto-feedback"
+              id="mailto-feedback"
+            >
+              <img src={mailtoIcon} alt="Email Icon" />
+              Feedback
+            </a>
           </li>
         </ul>
       </nav>
-      <div
-        className={`${styles.navBackdrop} ${isMenuOpen ? styles.open : ''}`}
-        role="button"
-        aria-roledescription="Dismiss Navigation Menu"
-        onClick={() => {
-          setIsMenuOpen(false);
-        }}
-      />
     </header>
   );
 }
+
+const feedbackTo = 'bikespaceto@gmail.com';
+const feedbackSubject = 'BikeSpace Dashboard Feedback';
+const feedbackBody = `----------
+Please describe your feedback about dashboard.bikespace.ca below. We welcome both positive feedback (e.g. I found x feature useful) and constructive feedback (e.g. y is broken, I wish the dashboard did z).
+
+Especially for constructive feedback, you can help us by letting us know:
+- Your browser and platform (e.g. Safari, iPhone)
+- If it's a bug, what steps led to the problem
+- If it's something you wish the dashboard was able to do, what goal would that feature help you accomplish? (e.g. "I wanted to see only issues along street x", or "I wanted to better understand issues of y type")
+
+Thank you for taking the time to help us make the dashboard better!
+----------
+
+Hi BikeSpace team,
+
+`;
