a1b2cd286ca78f3a0192086d001f477f28f82271
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-06-10 13:52:28 -0400

    fix plotly modebar button color

--- bikespace_frontend/src/config/plotly.ts
+++ bikespace_frontend/src/config/plotly.ts
@@ -4,7 +4,7 @@ export const layout = {
   dragmode: false,
   paper_bgcolor: 'transparent', // reset chart background to transparent to give more CSS control
   modebar: {
-    color: 'hsl(137, 56%, 73%)',
+    color: 'hsl(137, 68%, 23%)',
     activecolor: 'hsl(137, 68%, 45%)',
     bgcolor: 'transparent',
   },
