51979c07df94b21cdc89eead498d7c58ee82948a
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-06-16 14:33:07 -0400

    Added in validation details

--- datasets/bicycle_parking/bicycle_parking/assets/toronto_open_data.py
+++ datasets/bicycle_parking/bicycle_parking/assets/toronto_open_data.py
@@ -14,9 +14,18 @@ import geopandas as gpd
 import pandas as pd
 import pandera as pa
 
-from ..resources.toronto_open_data import TorontoOpenDataResource
+from ..resources.toronto_open_data import (
+  TorontoOpenDataResource, 
+  WARD_INFO,
+  BIKE_STATION_INFO,
+  )
 
 gpd.options.io_engine = "pyogrio"
+
+# type shorthands
+optional_string = pa.Column(str, required=False)
+optional_nullable_string = pa.Column(str, nullable=True, required=False)
+optional_int32 = pa.Column("int32", required=False)
 
 @asset
 def street_furniture_bicycle_parking(
@@ -44,31 +53,43 @@ def street_furniture_bicycle_parking(
   })
 
 @asset_check(asset=street_furniture_bicycle_parking)
-def validate_df_sfbp():
+def validate_sfbp():
   gdf = gpd.read_file("data/street_furniture_bicycle_parking.geojson")
   schema = pa.DataFrameSchema(
     {
-      "_id": pa.Column("int32", required=False),
-      "OBJECTID": pa.Column("int32", required=False),
-      "ID": pa.Column(str), #required
-      "ADDRESSNUMBERTEXT": pa.Column(str, nullable=True, required=False), 
-      "ADDRESSSTREET": pa.Column(str, nullable=True, required=False),  
-      "FRONTINGSTREET": pa.Column(str, nullable=True, required=False), 
-      "SIDE": pa.Column(str, nullable=True, required=False), 
-      "FROMSTREET": pa.Column(str, nullable=True, required=False), 
-      "DIRECTION": pa.Column(str, nullable=True, required=False), 
-      "SITEID": pa.Column(str, nullable=True, required=False), 
-      "WARD": pa.Column(str, nullable=True), #required
-      "BIA": pa.Column(str, nullable=True), #required
+      # REQUIRED FIELDS
+      "ID": pa.Column(str, unique=True,
+        checks=pa.Check.str_matches(r"BP-\d{5}") # e.g. BP-12345
+      ), 
+      "WARD": pa.Column(str, nullable=True,
+        checks=pa.Check.isin(list(
+          WARD_INFO["street_furniture_bicycle_parking"]["names"].keys()
+        )),
+      ), 
+      "BIA": pa.Column(str, nullable=True), 
       "ASSETTYPE": pa.Column(str, nullable=True, 
         checks=pa.Check.isin(
-          ['Ring', 'Rack', 'Art Stand', 'Shelter', 'Other', ''],
+          ["Ring", "Rack", "Art Stand", "Shelter", "Other"],
           ignore_na=True,
         ),
-      ), #required
-      "STATUS": pa.Column(str), #required
-      "SDE_STATE_ID": pa.Column("int32", required=False), 
+      ), 
+      "STATUS": pa.Column(str,
+        checks=pa.Check.isin(["Existing", "Temporarily Removed"]),
+      ),
       "geometry": pa.Column("geometry"),
+
+      # OTHER FIELDS
+      "_id": optional_int32,
+      # OBJECTID is not a primary key - inconsistent between updates
+      "OBJECTID": optional_int32,
+      "ADDRESSNUMBERTEXT": optional_nullable_string, 
+      "ADDRESSSTREET": optional_nullable_string,  
+      "FRONTINGSTREET": optional_nullable_string, 
+      "SIDE": optional_nullable_string, 
+      "FROMSTREET": optional_nullable_string, 
+      "DIRECTION": optional_nullable_string, 
+      "SITEID": optional_nullable_string, 
+      "SDE_STATE_ID": optional_int32, 
     },
     strict=True,
   )
@@ -88,6 +109,8 @@ def bicycle_parking_high_capacity_outdoor(
     resource_id="9dd8ada5-109d-4ca7-a25a-d0fe234b6710",
   ).values()
 
+  gdf = gdf.astype({"ID": str, "SIZE_M": float})
+
   os.makedirs("data", exist_ok=True)
   with open("data/bicycle_parking_high_capacity_outdoor.geojson", "w") as f:
     f.write(gdf.to_json(na='drop', drop_id=True, indent=2))
@@ -102,34 +125,56 @@ def bicycle_parking_high_capacity_outdoor(
   })
 
 @asset_check(asset=bicycle_parking_high_capacity_outdoor)
-def validate_df_bphco():
+def validate_bphco():
   gdf = gpd.read_file("data/bicycle_parking_high_capacity_outdoor.geojson")
   schema = pa.DataFrameSchema(
     {
-      "ADDRESS_POINT_ID": pa.Column("int32"),
-      "ADDRESS_NUMBER": pa.Column(str),
-      "LINEAR_NAME_FULL": pa.Column(str),
-      "ADDRESS_FULL": pa.Column(str),
-      "POSTAL_CODE": pa.Column(str, nullable=True),
-      "MUNICIPALITY": pa.Column(str),
-      "CITY": pa.Column(str),
+      # REQUIRED FIELDS
+      "ID": pa.Column(str, coerce=True, unique=True,
+        checks=pa.Check.str_matches(r"\d+"), # e.g. "1", "13"
+      ),
+      "MUNICIPALITY": pa.Column(str,
+        checks=pa.Check.isin([
+          "Etobicoke", 
+          "North York", 
+          "Scarborough", 
+          "York", 
+          "East York", 
+          "former Toronto"
+        ]),
+      ),
       "WARD": pa.Column(str),
       "PLACE_NAME": pa.Column(str, nullable=True),
-      "GENERAL_USE_CODE": pa.Column("int32"),
-      "CENTRELINE_ID": pa.Column("int32"),
-      "LO_NUM": pa.Column("int32"),
-      "LO_NUM_SUF": pa.Column(str, nullable=True),
-      "HI_NUM": pa.Column("int32"),
-      "LINEAR_NAME_ID": pa.Column("int32"),
-      "ID": pa.Column("int32"),
-      "PARKING_TYPE": pa.Column(str),
+      "PARKING_TYPE": pa.Column(str, 
+        checks=pa.Check.isin(
+          ["Bike Rack", "Angled Bike Rack", "Bike Corral", "Bike Shelter"]
+        ),
+      ),
       "FLANKING": pa.Column(str, nullable=True),
       "BICYCLE_CAPACITY": pa.Column("int32"),
-      "SIZE_M": pa.Column(str),
+      "SIZE_M": pa.Column(float),
       "YEAR_INSTALLED": pa.Column("int32"),
-      "BY_LAW": pa.Column(str),
       "DETAILS": pa.Column(str, nullable=True),
       "geometry": pa.Column("geometry"),
+
+      # OTHER FIELDS
+      "ADDRESS_POINT_ID": optional_int32,
+      "ADDRESS_NUMBER": optional_string,
+      "LINEAR_NAME_FULL": optional_string,
+      "ADDRESS_FULL": optional_string,
+      "POSTAL_CODE": optional_nullable_string,
+      "CITY": pa.Column(str, required=False,
+        checks=pa.Check.equal_to("Toronto"),
+      ),
+      "GENERAL_USE_CODE": optional_int32,
+      "CENTRELINE_ID": optional_int32,
+      "LO_NUM": optional_int32,
+      "LO_NUM_SUF": optional_nullable_string,
+      "HI_NUM": optional_int32,
+      "LINEAR_NAME_ID": optional_int32,
+      "BY_LAW": pa.Column(str, required=False,
+        checks=pa.Check.isin(["N/A", "NO", "Y"]),
+      ),
     }
   )
   test = type(schema.validate(gdf)) == gpd.GeoDataFrame
@@ -148,6 +193,9 @@ def bicycle_parking_racks(
     resource_id="4d105465-6e64-4a69-957b-6e0eee5bca8b",
   ).values()
 
+  gdf = gdf.astype({"OBJECTID": str})
+  gdf = gdf.replace(" ", pd.NA).replace("", pd.NA)
+
   os.makedirs("data", exist_ok=True)
   with open("data/bicycle_parking_racks.geojson", "w") as f:
     f.write(gdf.to_json(na='drop', drop_id=True, indent=2))
@@ -162,36 +210,71 @@ def bicycle_parking_racks(
   })
 
 @asset_check(asset=bicycle_parking_racks)
-def validate_df_bpr():
+def validate_bpr():
   gdf = gpd.read_file("data/bicycle_parking_racks.geojson")
   schema = pa.DataFrameSchema(
     {
-      "ADDRESS_POINT_ID": pa.Column("int32"),
-      "ADDRESS_NUMBER": pa.Column(str),
-      "LINEAR_NAME_FULL": pa.Column(str),
-      "ADDRESS_FULL": pa.Column(str),
-      "POSTAL_CODE": pa.Column(str),
-      "MUNICIPALITY": pa.Column(str),
-      "CITY": pa.Column(str),
-      "CENTRELINE_ID": pa.Column("int32"),
-      "LO_NUM": pa.Column("int32"),
-      "LO_NUM_SUF": pa.Column(str),
-      "HI_NUM": pa.Column("int32"),
-      "HI_NUM_SUF": pa.Column(str),
-      "LINEAR_NAME_ID": pa.Column("int32"),
-      "WARD_NAME": pa.Column(str),
-      "MI_PRINX": pa.Column("int32"),
-      "OBJECTID": pa.Column("int32"),
+      # REQUIRED FIELDS
+      "OBJECTID": pa.Column(str, unique=True),
+      "MUNICIPALITY": pa.Column(str,
+        checks=pa.Check.isin([
+          'ETOBICOKE', 
+          'NORTH YORK', 
+          'SCARBOROUGH', 
+          'YORK', 
+          'EAST YORK', 
+          'former TORONTO'
+        ]),
+      ),
+      "WARD_NAME": pa.Column(str, 
+        checks=pa.Check.str_matches(
+          WARD_INFO["bicycle_parking_racks"]["pattern"]
+        ),
+      ),
       "CAPACITY": pa.Column("int32"),
-      "MULTIMODAL": pa.Column(str),
-      "SEASONAL": pa.Column(str),
-      "SHELTERED": pa.Column(str),
-      "SURFACE": pa.Column(str),
-      "STATUS": pa.Column(str),
-      "LOCATION": pa.Column(str),
-      "NOTES": pa.Column(str),
-      "MAP_CLASS": pa.Column(str),
+      "SEASONAL": pa.Column(str,
+        checks=pa.Check.isin(["No", "Yes"]),
+      ),  
+      "SHELTERED": pa.Column(str, nullable=True,
+        checks=pa.Check.isin(["No", "Yes"]),
+      ),
+      "STATUS": pa.Column(str,
+        checks=pa.Check.isin(
+          ["Delivered", "Installed", "Approved", "Proposed", "TBD"]
+        ),
+      ),
+      "LOCATION": pa.Column(str, nullable=True),
+      "NOTES": pa.Column(str, nullable=True),
+      "MAP_CLASS": pa.Column(str,
+        checks=pa.Check.isin([
+          'Multi-Bike Rack', 
+          'Multi-Bike Rack (Angled)', 
+          'Bike Shelter', 
+          'Bike Corral'
+        ]),
+      ),
       "geometry": pa.Column("geometry"),
+
+      # OTHER FIELDS
+      "ADDRESS_POINT_ID": optional_int32,
+      "ADDRESS_NUMBER": optional_string,
+      "LINEAR_NAME_FULL": optional_string,
+      "ADDRESS_FULL": optional_string,
+      "POSTAL_CODE": optional_nullable_string,
+      "CITY": pa.Column(str, required=False,
+        checks=pa.Check.equal_to("Toronto"),
+      ),
+      "CENTRELINE_ID": optional_int32,
+      "LO_NUM": optional_int32,
+      "LO_NUM_SUF": optional_nullable_string,
+      "HI_NUM": optional_int32,
+      "HI_NUM_SUF": optional_string,
+      "LINEAR_NAME_ID": optional_int32,
+      "MI_PRINX": optional_int32,
+      "MULTIMODAL": pa.Column(str, required=False,
+        checks=pa.Check.isin(["No", "Yes"]),
+      ),
+      "SURFACE": optional_nullable_string,
     }
   )
   test = type(schema.validate(gdf)) == gpd.GeoDataFrame
@@ -210,6 +293,8 @@ def bicycle_parking_bike_stations_indoor(
     resource_id="c7829eee-131c-430d-a8de-a0b509b6d8f2",
   ).values()
 
+  gdf = gdf.astype({"ID": str})
+
   os.makedirs("data", exist_ok=True)
   with open("data/bicycle_parking_bike_stations_indoor.geojson", "w") as f:
     f.write(gdf.to_json(na='drop', drop_id=True, indent=2))
@@ -224,35 +309,65 @@ def bicycle_parking_bike_stations_indoor(
   })
 
 @asset_check(asset=bicycle_parking_bike_stations_indoor)
-def validate_df_bpbsi():
+def validate_bpbsi():
   gdf = gpd.read_file("data/bicycle_parking_bike_stations_indoor.geojson")
   schema = pa.DataFrameSchema(
     {
-      "ADDRESS_POINT_ID": pa.Column("int32"),
+      # REQUIRED FIELDS
+      "ID": pa.Column(str, coerce=True, unique=True,
+        checks=pa.Check.str_matches(r"\d+"), # e.g. "1", "13"
+      ),
       "ADDRESS_NUMBER": pa.Column(str),
       "LINEAR_NAME_FULL": pa.Column(str),
-      "ADDRESS_FULL": pa.Column(str),
+      "ADDRESS_FULL": pa.Column(str, 
+        checks=pa.Check.isin(list(BIKE_STATION_INFO.keys()))
+      ),
       "POSTAL_CODE": pa.Column(str, nullable=True),
-      "MUNICIPALITY": pa.Column(str),
-      "CITY": pa.Column(str),
-      "WARD_NAME": pa.Column(str),
+      "MUNICIPALITY": pa.Column(str,
+        checks=pa.Check.isin([
+          "Etobicoke", 
+          "North York", 
+          "Scarborough", 
+          "York", 
+          "East York", 
+          "former Toronto"
+        ]),
+      ),
+      "CITY": pa.Column(str,
+        checks=pa.Check.equal_to("Toronto"),
+      ),
+      "WARD_NAME": pa.Column(str, 
+        checks=pa.Check.str_matches(
+          WARD_INFO["bicycle_parking_bike_stations_indoor"]["pattern"]
+        ),
+      ),
       "PLACE_NAME": pa.Column(str, nullable=True),
-      "GENERAL_USE_CODE": pa.Column("int32"),
-      "CENTRELINE_ID": pa.Column("int32"),
-      "LO_NUM": pa.Column("int32"),
-      "HI_NUM": pa.Column("int32"),
-      "LINEAR_NAME_ID": pa.Column("int32"),
-      "MI_PRINX": pa.Column("int32"),
-      "ID": pa.Column("int32"),
-      "STATION_TYPE": pa.Column(str),
+      "STATION_TYPE": pa.Column(str,
+        checks=pa.Check.equal_to("Bicycle Station"),
+      ),
       "TRANSIT_STATION": pa.Column(str),
       "FLANKING": pa.Column(str),
       "BIKE_CAPACITY": pa.Column("int32"),
-      "CHANGE_ROOM": pa.Column(str),
-      "VENDING_MACHINE": pa.Column(str),
-      "TOOLS_PUMP": pa.Column(str),
+      "CHANGE_ROOM": pa.Column(str, 
+        checks=pa.Check.isin(["No", "Yes"]),
+      ),
+      "VENDING_MACHINE": pa.Column(str, 
+        checks=pa.Check.isin(["No", "Yes"]),
+      ),
+      "TOOLS_PUMP": pa.Column(str, 
+        checks=pa.Check.isin(["No", "Yes"]),
+      ),
       "YR_INSTALL": pa.Column("int32"),
       "geometry": pa.Column("geometry"),
+
+      # OTHER FIELDS
+      "ADDRESS_POINT_ID": optional_int32,
+      "GENERAL_USE_CODE": optional_int32,
+      "CENTRELINE_ID": optional_int32,
+      "LO_NUM": optional_int32,
+      "HI_NUM": optional_int32,
+      "LINEAR_NAME_ID": optional_int32,
+      "MI_PRINX": optional_int32,
     }
   )
   test = type(schema.validate(gdf)) == gpd.GeoDataFrame
--- datasets/bicycle_parking/bicycle_parking/resources/toronto_open_data.py
+++ datasets/bicycle_parking/bicycle_parking/resources/toronto_open_data.py
@@ -33,3 +33,71 @@ class TorontoOpenDataResource(ConfigurableResource):
        "gdf": gdf,
        "metadata": meta_resource,
     }
+
+WARD_INFO = {
+   "street_furniture_bicycle_parking": {
+      "names": {
+        "01": "Etobicoke North",
+        "02": "Etobicoke Centre",
+        "03": "Etobicoke-Lakeshore",
+        "04": "Parkdale-High Park",
+        "05": "York South-Weston",
+        "06": "York Centre",
+        "07": "Humber River-Black Creek",
+        "08": "Eglinton-Lawrence",
+        "09": "Davenport",
+        "10": "Spadina-Fort York",
+        "11": "University-Rosedale",
+        "12": "Toronto-St. Paul’s",
+        "13": "Toronto Centre",
+        "14": "Toronto-Danforth",
+        "15": "Don Valley West",
+        "16": "Don Valley East",
+        "17": "Don Valley North",
+        "18": "Willowdale",
+        "19": "Beaches-East York",
+        "20": "Scarborough Southwest",
+        "21": "Scarborough Centre",
+        "22": "Scarborough-Agincourt",
+        "23": "Scarborough North",
+        "24": "Scarborough-Guildwood",
+        "25": "Scarborough-Rouge Park"
+      },
+   },
+   "bicycle_parking_racks": {
+      "pattern": r"\s*?(?P<ward_name>.+?)\s*?\(\s*?\d+(?P<ward_number>\d{2})\s*?\)",
+   },
+   "bicycle_parking_bike_stations_indoor": {
+      "pattern": r"\s*?(?P<ward_name>.+?)\s*?\(\s*?\d+(?P<ward_number>\d{2})\s*?\)",
+   },
+}
+
+# Additional key details derived from the City of Toronto webpage, accessed 2024-06 from https://www.toronto.ca/services-payments/streets-parking-transportation/cycling-in-toronto/bicycle-parking/bicycle-parking-stations/
+# key values are 'ADDRESS_FULL'
+BIKE_STATION_INFO = {
+  # Union Station - Old?
+  '25 York St': {
+     "description": """MAY BE DEFUNCT - NEEDS CONFIRMATION""",
+     "capacity": 0,
+  },
+  # Victoria Park
+  '777 Victoria Park Ave': {
+    "description": """Located at the main entrance to Victoria Park Subway Station, the bicycle station offers secure, 24-hour bicycle parking with 52 bicycle parking spaces available on two-tier racks.""",
+    "capacity": 52,
+  }, 
+  # Union Station
+  '97 Front St W': {
+    "description": """The Bicycle Station is located on the east side of York Street, just south of Front Street. The facility features [approximately 160] bike racks, a washroom, a change room and  a shower with complimentary towels, and an office where staff can register new members and renew bike station parking plans. Tools and pumps are available for members to perform minor repairs.""",
+    "capacity": 168,
+  },
+  # City Hall / Nathan Phillips Square
+  '100 Queen St W': {
+    "description": """The Nathan Phillips Square Bicycle Station is located in the underground parking facility at Toronto City Hall, 100 Queen Street West. The facility opened May 6, 2019 and includes 170 bicycle parking spaces, washrooms and showers, and a staff office. To visit the bike station on foot, use the “Squirrel” stairs or elevator, located between the skate rental and the snacks concession, to get to the P1 level of the parking garage, then follow the signs. Once you have access as a Bicycle Station member, you can ride your bike down the vehicle entrance on the north side of Queen St., just east of York St.  You can also wheel your bike down the stair channel on the Pedestrian Southbound Concourse stairway, on the sidewalk near where the food vendor trucks park on Queen Street.""",
+    "capacity": 170,
+  },
+  # Finch West  
+  '3955 Keele St': {
+    "description": """The new TTC station at Finch and Keele features a bicycle station with secure parking for 68 bicycles. The facility opened to the public in October of 2018.""",
+    "capacity": 68,
+  },
+}
