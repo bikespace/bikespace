561c5fede046f0f0e9a877ec74c59c6966d7fb9f
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-06-17 17:33:23 -0400

    fix @testing-library/jest-dom config so import is not required in every test file

--- bikespace_frontend/src/components/dashboard/sidebar-tabs/sidebar-tabs.spec.tsx
+++ bikespace_frontend/src/components/dashboard/sidebar-tabs/sidebar-tabs.spec.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render, screen, fireEvent} from '@testing-library/react';
-import '@testing-library/jest-dom';
 
 import {SidebarTab} from '../context';
 
--- bikespace_frontend/src/components/submission/issue/issue.spec.tsx
+++ bikespace_frontend/src/components/submission/issue/issue.spec.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import {render, screen} from '@testing-library/react';
-import '@testing-library/jest-dom';
 
 import {IssueType} from '@/interfaces/Submission';
 
