692c1fca1ddbebdb358cdb5b40503d03c9eb6cbc
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-06-17 13:45:31 -0400

    refactor template into own component folder

--- bikespace_frontend/src/pages/{markdownRemark.frontmatter__slug}.tsx
+++ bikespace_frontend/src/pages/{markdownRemark.frontmatter__slug}.tsx
@@ -1,37 +1,8 @@
-import * as React from 'react';
 import {graphql} from 'gatsby';
 
-import {PageHeader} from '@/components/md-page/page-header/PageHeader';
+import {PageTemplate} from '@/components/md-page';
 
-import * as styles from '@/components/md-page/page.module.scss';
-
-type PageData = {
-  markdownRemark: {
-    frontmatter: {
-      slug: string;
-      title: string;
-    };
-    html: string;
-  };
-};
-
-export default function PageTemplate({
-  data, // this prop will be injected by the GraphQL query below.
-}: {
-  data: PageData;
-}) {
-  const {markdownRemark} = data; // data.markdownRemark holds your post data
-  const {html} = markdownRemark;
-  return (
-    <>
-      <PageHeader />
-      <main className={styles.pageMain}>
-        <div dangerouslySetInnerHTML={{__html: html}} />
-      </main>
-      <footer className={styles.pageFooter}>This will be a footer</footer>
-    </>
-  );
-}
+export default PageTemplate;
 
 export const pageQuery = graphql`
   query ($id: String!) {
