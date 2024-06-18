118c20281aff9fa1cfe4adea46bd06d3925fca0a
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-06-17 21:49:18 -0400

    add userEvent test to improve coverage for comments

--- bikespace_frontend/src/components/submission/comments/comments.spec.tsx
+++ bikespace_frontend/src/components/submission/comments/comments.spec.tsx
@@ -1,18 +1,30 @@
-import React, {useState} from 'react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
 import {render, screen} from '@testing-library/react';
+import {faker} from '@faker-js/faker';
 
 import {Comments} from './Comments';
 
 describe('Comments', () => {
-  const [comments, setComments] = useState('');
-
   test('Comments title should be rendered correctly', () => {
-    render(<Comments comments={comments} onCommentsChanged={setComments} />);
+    render(<Comments comments="" onCommentsChanged={jest.fn()} />);
     expect(screen.getByRole('heading', {level: 1})).toHaveTextContent(
       'Comments'
     );
     expect(screen.getByRole('heading', {level: 3})).toHaveTextContent(
       'Any additional comments you want to add...'
     );
+  });
+
+  test('Dispatch action should be triggered when user types', async () => {
+    const onCommentsChanged = jest.fn();
+
+    render(<Comments comments="" onCommentsChanged={onCommentsChanged} />);
+
+    const text = faker.string.alpha();
+
+    const textarea = screen.getByRole('textbox');
+    await userEvent.type(textarea, text);
+    expect(onCommentsChanged).toHaveBeenCalledTimes(text.length);
   });
 });
