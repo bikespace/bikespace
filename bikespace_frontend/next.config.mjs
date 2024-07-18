aac830464e6178234b0f430c9bdc927cf9a102bc
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-07-18 10:59:00 -0400

    fix next mdx integration

--- bikespace_frontend/next.config.mjs
+++ bikespace_frontend/next.config.mjs
@@ -1,11 +1,11 @@
 import withMDX from '@next/mdx';
 
-const nextConfig = withMDX({
+const nextConfig = withMDX()({
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   experimental: {
     typedRoutes: true,
   },
-})();
+});
 
 export default nextConfig;
