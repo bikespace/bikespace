ba912b2e21c9f79f50cf3d8df1591d5206e86827
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-07-18 12:58:23 -0400

    revert to old coverage config

--- bikespace_frontend/jest.config.ts
+++ bikespace_frontend/jest.config.ts
@@ -28,10 +28,7 @@ const config: Config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: [
-    "src/**/*.{ts,tsx}",
-    "!src/**/*.d.ts"
-  ],
+  collectCoverageFrom: [],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
