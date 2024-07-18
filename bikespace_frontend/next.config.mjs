6dbaa186f27d1e31424d47ada69f76a25a7d8853
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-07-18 09:40:31 -0400

    add mdx config for nextjs

--- bikespace_frontend/next.config.mjs
+++ bikespace_frontend/next.config.mjs
@@ -1,7 +1,11 @@
-const nextConfig = {
+import withMDX from '@next/mdx';
+
+const nextConfig = withMDX({
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   experimental: {
     typedRoutes: true,
   },
-};
+});
 
 export default nextConfig;
