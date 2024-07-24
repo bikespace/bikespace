a864cbed5840aaae1418f1ccb1d6b5c49a07593c
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-07-24 11:02:23 -0400

    FIX: Opening drawer on feed view covers entire page instead of half

--- bikespace_frontend/src/components/dashboard/sidebar/Sidebar.tsx
+++ bikespace_frontend/src/components/dashboard/sidebar/Sidebar.tsx
@@ -9,14 +9,14 @@ export function Sidebar() {
   const [open, setOpen] = useState<boolean>(true);
 
   return (
-    <div className={styles.sidebar}>
+    <div className={`${styles.sidebar} ${open ? '' : styles.closed}`}>
       <button
         className={styles.drawerHandle}
         onClick={() => {
           setOpen(prev => !prev);
         }}
       />
-      <div className={`${styles.sidebarContent} ${open ? '' : styles.closed}`}>
+      <div className={styles.sidebarContent}>
         <SidebarTabs />
         <SidebarTabContent />
       </div>
