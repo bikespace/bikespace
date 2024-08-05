d2a6111934cb22f4f480727d45eae0a63b750d64
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-08-04 23:19:42 -0400

    Update Submission.tsx
    Should map to Date.prototype.getDay() where 0 represents Sunday

--- bikespace_frontend/src/interfaces/Submission.tsx
+++ bikespace_frontend/src/interfaces/Submission.tsx
@@ -82,13 +82,13 @@ export interface SubmissionFilters {
 }
 
 export enum Day {
-  Monday = 0,
-  Tuesday = 1,
-  Wednesday = 2,
-  Thursday = 3,
-  Friday = 4,
-  Saturday = 5,
-  Sunday = 6,
+  Monday = 1,
+  Tuesday = 2,
+  Wednesday = 3,
+  Thursday = 4,
+  Friday = 5,
+  Saturday = 6,
+  Sunday = 0,
 }
 
 export type SubmissionsDateRange = {
