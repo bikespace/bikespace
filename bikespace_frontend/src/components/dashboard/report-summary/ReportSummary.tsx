a3fd9e94bc87f22cdba49e0ebffea2812e63d141
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-08-05 15:23:41 -0400

    Update ReportSummary.tsx
    Added loading indicator

--- bikespace_frontend/src/components/dashboard/report-summary/ReportSummary.tsx
+++ bikespace_frontend/src/components/dashboard/report-summary/ReportSummary.tsx
@@ -3,13 +3,15 @@ import {DateTime} from 'luxon';
 
 import {useSubmissionsStore} from '@/store';
 
-import {useAllSubmissionsDateRange} from '@/hooks';
+import {useAllSubmissionsDateRange, useSubmissionsQuery} from '@/hooks';
 
 import warningIcon from '@/assets/icons/exclamation-triangle.svg';
 
 import styles from './report-summary.module.scss';
 
 export function ReportSummary() {
+  const {isPending} = useSubmissionsQuery();
+
   const {submissions, filters} = useSubmissionsStore(state => ({
     submissions: state.submissions,
     filters: state.filters,
@@ -43,7 +45,9 @@ export function ReportSummary() {
   return (
     <div className={styles.summary}>
       <div>
-        {submissions.length > 0 ? (
+        {isPending ? (
+          <span className={styles.entryCount}>Loading Reports...</span>
+        ) : submissions.length > 0 ? (
           <span className={styles.entryCount}>
             {submissions.length.toLocaleString('en-CA')}
           </span>
@@ -55,23 +59,27 @@ export function ReportSummary() {
           />
         )}
         <span>
-          {submissions.length > 0
-            ? ` reports ${
-                filters.dateRange === null &&
-                filters.dateRangeInterval === null &&
-                filters.issue === null &&
-                filters.parkingDuration.length === 0 &&
-                filters.day === null
-                  ? ''
-                  : ' (filtered)'
-              }`
-            : 'No reports match filter criteria'}
+          {isPending
+            ? ''
+            : submissions.length > 0
+              ? ` reports ${
+                  filters.dateRange === null &&
+                  filters.dateRangeInterval === null &&
+                  filters.issue === null &&
+                  filters.parkingDuration.length === 0 &&
+                  filters.day === null
+                    ? ''
+                    : ' (filtered)'
+                }`
+              : 'No reports match filter criteria'}
         </span>
       </div>
       <div>
-        {submissions.length > 0
-          ? `${earliestEntry} - ${latestEntry}`
-          : 'Date Range N/A'}
+        {isPending
+          ? ''
+          : submissions.length > 0
+            ? `${earliestEntry} - ${latestEntry}`
+            : 'Date Range N/A'}
       </div>
     </div>
   );
