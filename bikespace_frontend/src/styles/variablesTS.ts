b1863f5d6a59cf2d8476e5fdb92065f6d8373613
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-10-05 10:51:39 -0400

    Changed breakpoint to be only defined in scss

--- bikespace_frontend/src/styles/variablesTS.ts
+++ bikespace_frontend/src/styles/variablesTS.ts
@@ -1,4 +1,8 @@
 // Style variables for use in TSX files - should match CSS variables
-const wrapperFullWidth: number = 1024;
+import styleVars from './variables.module.scss';
+
+// const wrapperFullWidth: number = 1024;
+const wrapperFullWidth = parseInt(styleVars.wrapperFullWidth);
+console.log('width', wrapperFullWidth, styleVars);
 
 export {wrapperFullWidth};
