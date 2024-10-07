9c05b935d21640bfc4ac4e4c005564233ac20ff0
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-10-07 13:15:46 -0400

    Dashboard UI cleanup (#189)
    * Prevent initial geolocate if issue specified by url param
    
    * Standardize breakpoint to 1024px
    
    * Changed breakpoint to be only defined in scss using one variable 
    
    * Switch to feed on mobile if issue specified in url param on load

--- bikespace_frontend/src/components/dashboard/map-marker/MapMarker.tsx
+++ bikespace_frontend/src/components/dashboard/map-marker/MapMarker.tsx
@@ -22,7 +22,7 @@ import {issuePriority} from '@/config/bikespace-api';
 
 import {trackUmamiEvent} from '@/utils';
 
-import {SidebarTab, useSubmissionId} from '@/states/url-params';
+import {SidebarTab, useSidebarTab, useSubmissionId} from '@/states/url-params';
 
 import {MapPopup} from '../map-popup';
 
@@ -34,6 +34,7 @@ import otherIcon from '@/assets/icons/icon_other.svg';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 import styles from './map-marker.module.scss';
+import {wrapperFullWidth} from '@/styles/variablesTS';
 
 interface MapMarkerProps {
   submission: SubmissionApiPayload;
@@ -53,6 +54,7 @@ const MapMarker = forwardRef(
     const innerMarkerRef = useRef<LeafletMarker>(null);
     // pass MarkerRef to parent while also allowing it to be used in this component:
     useImperativeHandle(outerMarkerRef, () => innerMarkerRef.current!, []);
+    const [, setTab] = useSidebarTab();
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const {replace} = useRouter();
@@ -67,6 +69,9 @@ const MapMarker = forwardRef(
     // omits dependencies array to run on every render
     useEffect(() => {
       if (!isFocused || !doneLoading) return;
+      if (windowWidth && windowWidth <= wrapperFullWidth) {
+        setTab(SidebarTab.Feed);
+      }
       clusterRef.current!.zoomToShowLayer(innerMarkerRef.current!, () => {
         innerMarkerRef.current!.openPopup();
       });
@@ -83,7 +88,7 @@ const MapMarker = forwardRef(
     };
 
     const handleClick = () => {
-      if (windowWidth && windowWidth <= 768) {
+      if (windowWidth && windowWidth <= wrapperFullWidth) {
         // Manually set tab= URL params to prevent excess rerendering from subscribing to tab change
         const params = new URLSearchParams(searchParams);
 
