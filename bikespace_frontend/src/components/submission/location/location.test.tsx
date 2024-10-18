ec671c4f9f82ec79c10996c0c0b74ee9cd9a99ae
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-10-18 15:02:03 -0400

    fix location test file

--- bikespace_frontend/src/components/submission/location/location.test.tsx
+++ bikespace_frontend/src/components/submission/location/location.test.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import {faker} from '@faker-js/faker';
+import {FormProvider, useForm} from 'react-hook-form';
+
+import {SubmissionSchema} from '../schema';
 
 import {Location} from './Location';
 
+const MockForm = () => {
+  const form = useForm<SubmissionSchema>({
+    defaultValues: {
+      location: {
+        latitude: 43.65322,
+        longitude: -79.384452,
+      },
+    },
+  });
+
+  return (
+    <FormProvider {...form}>
+      <form>
+        <Location handler={<></>} />
+      </form>
+    </FormProvider>
+  );
+};
+
 describe('Test Location page component', () => {
   test('Title should be rendered properly', () => {
-    render(
-      <Location
-        location={{
-          latitude: faker.location.latitude(),
-          longitude: faker.location.longitude(),
-        }}
-        handler={<></>}
-      />
-    );
+    render(<MockForm />);
 
     expect(screen.getByRole('heading', {level: 2})).toHaveTextContent(
       'Where was the problem?'
