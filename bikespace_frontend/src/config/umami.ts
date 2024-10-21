68266b343dc85ae74d0ce19b3c8de59921528043
Ben Coleman <ben.coleman@alumni.utoronto.ca>
2024-10-21 10:07:58 -0400

    Fixed umami ID used on frontend (#194)
    - also disabled umami load on develop
    - also fixed github contribute link on new dashboard

--- bikespace_frontend/src/config/umami.ts
+++ bikespace_frontend/src/config/umami.ts
@@ -1,4 +1,4 @@
 export const umamiConfig = {
-  websiteId: '77b33a3d-0b83-41f2-9b54-589d36903aed',
+  websiteId: 'd56aa43e-3009-4f6d-9975-c82af706c3be',
   hostUrl: 'https://us.umami.is/script.js',
 };
