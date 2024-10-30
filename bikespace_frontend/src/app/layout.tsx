5dee36a3cef178d5811ef1858727ea99af3c71b8
Sonal Ranjit <sonal.ranjit3@gmail.com>
2024-10-30 12:55:57 +0000

    Use public folder for web manifest setup

--- bikespace_frontend/src/app/layout.tsx
+++ bikespace_frontend/src/app/layout.tsx
@@ -22,6 +22,23 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Bikespace" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>{children}</body>
       {
         // Disable tracking during development
