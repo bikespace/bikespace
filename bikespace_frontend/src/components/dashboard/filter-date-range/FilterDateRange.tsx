088f36e4c4370f82f144088b42eb1d3ca7c01b9e
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-12-14 12:47:34 -0500

    Simplified FilterDateRange
    - use computed value to show/hide custom date picker
    - separate handling function instead of inline
    - more conditional parts of component separate instead of inline
    - simpler display for "all dates shown"

--- bikespace_frontend/src/components/dashboard/filter-date-range/FilterDateRange.tsx
+++ bikespace_frontend/src/components/dashboard/filter-date-range/FilterDateRange.tsx
@@ -1,19 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {DateTime} from 'luxon';
-
 import {DateRangeInterval} from '@/interfaces/Submission';
-
 import {trackUmamiEvent} from '@/utils';
-
 import {useAllSubmissionsDateRange} from '@/hooks';
-
 import {useSubmissionsStore} from '@/states/store';
-
-import {getDateRangeFromInterval} from './utils';
-
+import {getDateRangeFromInterval} from './UtilsFilterDateRange';
 import {FilterSection} from '../filter-section';
 import {FilterDateRangeCustom} from '../filter-date-range-custom';
-
 import styles from './filter-date-range.module.scss';
 
 export function FilterDateRange() {
@@ -26,10 +19,37 @@ export function FilterDateRange() {
   );
   const {first, last} = useAllSubmissionsDateRange();
 
-  const [showCustomRange, setShowCustomRange] = useState<boolean>(false);
+  function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    const value = e.target.value as DateRangeInterval;
 
-  return (
-    <FilterSection title="Date Range">
+    if (value === DateRangeInterval.CustomRange) {
+      setFilters({
+        dateRange: dateRange,
+        dateRangeInterval: value,
+      });
+    } else {
+      const newDateRange = getDateRangeFromInterval(value);
+
+      setFilters({
+        dateRange: newDateRange,
+        dateRangeInterval: value,
+      });
+
+      trackUmamiEvent('datefilter', {
+        from: newDateRange ? newDateRange.from : '',
+        to: newDateRange ? newDateRange.to : '',
+        interval: value,
+      });
+    }
+  }
+
+  const rangeIndicator =
+    (dateRangeInterval ?? DateRangeInterval.AllDates) ===
+    DateRangeInterval.AllDates ? (
+      <div>
+        <strong>Showing all dates</strong>
+      </div>
+    ) : (
       <div>
         <div>
           <strong>Showing between:</strong>
@@ -46,37 +66,25 @@ export function FilterDateRange() {
           )}`}
         </div>
       </div>
+    );
+
+  const customRange =
+    dateRangeInterval === DateRangeInterval.CustomRange ? (
+      <div>
+        <FilterDateRangeCustom />
+      </div>
+    ) : null;
+
+  return (
+    <FilterSection title="Date Range">
+      {rangeIndicator}
       <div className={styles.dateRangeSelect}>
-        <label htmlFor="filter-date-range-select">
-          <strong>Select:</strong>
-        </label>
+        <label htmlFor="filter-date-range-select">Select:</label>
         <select
           name="dateRange"
           id="filter-date-range-select"
-          value={dateRangeInterval || DateRangeInterval.AllDates}
-          onChange={e => {
-            const value = e.currentTarget.value as DateRangeInterval;
-
-            if (value === DateRangeInterval.CustomRange) {
-              setShowCustomRange(true);
-            } else {
-              setShowCustomRange(false);
-
-              const dateRange = getDateRangeFromInterval(value);
-
-              setFilters({
-                dateRange,
-                dateRangeInterval: value,
-              });
-
-              if (dateRange)
-                trackUmamiEvent('datefilter', {
-                  from: dateRange.from,
-                  to: dateRange.to,
-                  interval: value,
-                });
-            }
-          }}
+          value={dateRangeInterval ?? ''}
+          onChange={handleChange}
         >
           {dateRangeOptGroups.map(group => (
             <optgroup label={group.label} key={group.label}>
@@ -89,9 +97,7 @@ export function FilterDateRange() {
           ))}
         </select>
       </div>
-      <div hidden={!showCustomRange}>
-        <FilterDateRangeCustom />
-      </div>
+      {customRange}
     </FilterSection>
   );
 }
