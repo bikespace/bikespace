193445f6f14674702a855dc5abc6f24f495a5f93
Ben Coleman <ben.coleman@alum.utoronto.ca>
2025-02-12 22:25:15 -0500

    Fix schema used for api submissions POST

--- bikespace_frontend/src/components/submission/schema.ts
+++ bikespace_frontend/src/components/submission/schema.ts
@@ -1,6 +1,10 @@
 import * as z from 'zod';
 
-import {IssueType, ParkingDuration} from '@/interfaces/Submission';
+import {
+  IssueType,
+  ParkingDuration,
+  SubmissionPayload,
+} from '@/interfaces/Submission';
 import {useFormContext} from 'react-hook-form';
 
 export const submissionSchema = z.object({
@@ -23,3 +27,16 @@ export type SubmissionSchema = z.infer<typeof submissionSchema>;
 export const useSubmissionFormContext = () => {
   return useFormContext<SubmissionSchema>();
 };
+
+export function convertSubmissionSchemaToSubmissionPayload(
+  data: SubmissionSchema
+): SubmissionPayload {
+  return {
+    latitude: data.location.latitude,
+    longitude: data.location.longitude,
+    issues: data.issues,
+    parking_time: data.parkingTime.date,
+    parking_duration: data.parkingTime.parkingDuration,
+    comments: data.comments,
+  };
+}
