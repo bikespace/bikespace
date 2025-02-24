de82a5e0fdd61bf1b9294c2fa38400b09ce55b17
Ben Coleman <ben.coleman@alum.utoronto.ca>
2025-02-23 22:20:43 -0500

    Update DataIssueFrequencyChart.tsx
    Test of getting images onto the issue chart

--- bikespace_frontend/src/components/dashboard/data-issue-frequency-chart/DataIssueFrequencyChart.tsx
+++ bikespace_frontend/src/components/dashboard/data-issue-frequency-chart/DataIssueFrequencyChart.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useState, useEffect} from 'react';
 import Plotly, {PlotParams} from 'react-plotly.js';
-import {PlotMouseEvent} from 'plotly.js-dist-min';
+import {PlotMouseEvent, Image} from 'plotly.js-dist-min';
 
 import {layout, config} from '@/config/plotly';
 
@@ -11,6 +11,12 @@ import {trackUmamiEvent} from '@/utils';
 import {useSubmissionsStore} from '@/states/store';
 
 import styles from './data-issue-frequency-chart.module.scss';
+
+import notProvidedIcon from '@/assets/icons/icon_not_provided.svg';
+import abandonedIcon from '@/assets/icons/icon_abandoned.svg';
+import fullIcon from '@/assets/icons/icon_full.svg';
+import damagedIcon from '@/assets/icons/icon_damaged.svg';
+import otherIcon from '@/assets/icons/icon_other.svg';
 
 type InputData = {
   type: IssueType;
@@ -90,12 +96,13 @@ function DataIssueFrequencyChart({className}: Pick<PlotParams, 'className'>) {
         },
         margin: {
           t: 30,
-          r: 20,
+          r: 20, // 20
           b: 4,
-          l: 120,
+          l: 140, // 140
         },
         width: 320 - 4 * 2,
         height: 200,
+        images: issueImages,
       }}
       config={config}
       onClick={handleClick}
@@ -106,10 +113,40 @@ function DataIssueFrequencyChart({className}: Pick<PlotParams, 'className'>) {
 export default DataIssueFrequencyChart;
 export {DataIssueFrequencyChart};
 
+const spacer = '       ';
+
 const issueLabels = {
-  [IssueType.NotProvided]: 'No nearby parking ',
-  [IssueType.Damaged]: 'Parking damaged ',
-  [IssueType.Full]: 'Parking full ',
-  [IssueType.Abandoned]: 'Abandoned bicycle ',
-  [IssueType.Other]: 'Other issue ',
+  [IssueType.NotProvided]: 'No nearby parking ' + spacer,
+  [IssueType.Damaged]: 'Parking damaged ' + spacer,
+  [IssueType.Full]: 'Parking full ' + spacer,
+  [IssueType.Abandoned]: 'Abandoned bicycle ' + spacer,
+  [IssueType.Other]: 'Other issue ' + spacer,
 } satisfies {[key in IssueType]: string};
+
+const issueIcons = {
+  [IssueType.NotProvided]: notProvidedIcon.src,
+  [IssueType.Damaged]: damagedIcon.src,
+  [IssueType.Full]: fullIcon.src,
+  [IssueType.Abandoned]: abandonedIcon.src,
+  [IssueType.Other]: otherIcon.src,
+} satisfies {[key in IssueType]: string};
+
+const baseImage = {
+  // x: -((140 - 4 * 2) / (320 - 140)), // 1.05 for right
+  // x: 1.05,
+  x: -0.01,
+  sizex: 0.16,
+  sizey: 0.16,
+  xanchor: 'right', // right
+  yanchor: 'bottom',
+};
+
+const issueImages = Object.entries(issueIcons).map(([k, v], i) => {
+  return {
+    ...baseImage,
+    y: i / Object.values(issueIcons).length + 0.02,
+    source: v,
+  } as Partial<Image>;
+});
+
+console.log(issueImages);
