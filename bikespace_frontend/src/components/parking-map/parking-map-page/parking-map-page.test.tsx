ab1b0946c61006c36693907766c06dc1b792a23e
Ben Coleman <ben.coleman@alum.utoronto.ca>
2025-04-21 20:40:09 -0400

    Add mocks and test to ParkingMapPage test
    - added test to make sure all images on first load have alt text

--- bikespace_frontend/src/components/parking-map/parking-map-page/parking-map-page.test.tsx
+++ bikespace_frontend/src/components/parking-map/parking-map-page/parking-map-page.test.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import {render, screen} from '@testing-library/react';
 
 import {ParkingMapPage} from './ParkingMapPage';
+
+jest.mock('react-map-gl/maplibre', () => ({
+  __esModule: true,
+  default: forwardRef((props, ref) => <></>), // Map is default export
+  GeolocateControl: () => <></>,
+  NavigationControl: () => <></>,
+}));
 
 describe('ParkingMapPage', () => {
   beforeEach(() => {
     render(<ParkingMapPage />);
   });
 
-  test('render ParkingMapPage', () => {
-    screen.debug();
+  // test('render ParkingMapPage', () => {
+  //   screen.debug();
+  // });
+
+  test('All ParkingMapPage images on first load have alt text', () => {
+    expect(
+      screen
+        .getAllByRole('img')
+        .map(img => img.getAttribute('alt') ?? img.getAttribute('aria-label'))
+    ).toBeTruthy();
   });
 });
