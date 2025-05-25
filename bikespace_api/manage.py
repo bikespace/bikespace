729bcff0777e18a182526da2e6fa34a9fe477c05
Sonal Ranjit <sonal.ranjit3@gmail.com>
2025-05-25 15:25:32 -0400

    Working base version of flask-admin and flask-security

--- bikespace_api/manage.py
+++ bikespace_api/manage.py
@@ -4,12 +4,16 @@ import time
 from flask.cli import FlaskGroup
 from sqlalchemy_utils import database_exists, create_database
 
-from bikespace_api import create_app, db
-from bikespace_api.api.models import Submission, IssueType, ParkingDuration
+from bikespace_api import create_app, db, create_userdatastore 
+from bikespace_api.api.models import Submission, IssueType, ParkingDuration, User, Role
 from datetime import datetime
+from flask_security.utils import hash_password    
+import random
+import string
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+user_datastore = create_userdatastore(db, User, Role)
 
 
 @cli.command()
@@ -79,13 +83,55 @@ def seed_db():
             "Example of null submitted_datetime",
         )
     )
+    user_role = Role(name="user")
+    super_user_role = Role(name="superuser")
+    db.session.add(user_role)
+    db.session.add(super_user_role)
     db.session.commit()
+
+    user_datastore.create_user(
+        first_name="Admin",
+        email="admin@example.com",
+        password=hash_password("admin"),
+        roles=[user_role, super_user_role],
+    )
 
     # have to manually null out submitted_datetime to replicate grandfathered database entry
     submitted_datetime_null = db.session.execute(
         db.select(Submission).filter_by(comments="Example of null submitted_datetime")
     ).scalar_one()
     submitted_datetime_null.submitted_datetime = None
+    db.session.commit()
+
+    first_names = [
+            "Harry",
+            "Amelia",
+            "Oliver",
+            "Jack",
+    ]
+    last_names = [
+            "Brown",
+            "Smith",
+            "Patel",
+            "Jones",
+    ]
+
+    for i in range(len(first_names)):
+        tmp_email = (
+            first_names[i].lower() + "." + last_names[i].lower() + "@example.com"
+        )
+        tmp_pass = "".join(
+            random.choice(string.ascii_lowercase + string.digits) for i in range(10)
+        )
+        user_datastore.create_user(
+            first_name=first_names[i],
+            last_name=last_names[i],
+            email=tmp_email,
+            password=hash_password(tmp_pass),
+            roles=[
+                user_role,
+            ],
+        )
     db.session.commit()
 
 
