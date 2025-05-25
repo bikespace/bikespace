fb96f8e6de7936d7e19b244a4155efbfb684d686
Sonal Ranjit <sonal.ranjit3@gmail.com>
2025-05-25 15:25:32 -0400

    Working base version of flask-admin and flask-security

--- bikespace_api/bikespace_api/__init__.py
+++ bikespace_api/bikespace_api/__init__.py
@@ -2,15 +2,22 @@
 
 import os
 
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, url_for
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin import helpers as admin_helpers
+from flask_admin.contrib.sqla import ModelView
+from flask_security import Security, SQLAlchemyUserDatastore
+from flask_security import RoleMixin, UserMixin
 
 # instantiate the db
 db = SQLAlchemy()
 migrate = Migrate()
 
+def create_userdatastore(db: SQLAlchemy, user_model: UserMixin, role_model: RoleMixin):
+    return SQLAlchemyUserDatastore(db, user_model, role_model)
 
 def create_app(script_info=None):
     # instantiante the app
@@ -21,16 +28,43 @@ def create_app(script_info=None):
     app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
 
+
     # set up extensions
     db.init_app(app)
     migrate.init_app(app, db)
 
+
     # register blueprints
     from bikespace_api.api.submissions import submissions_blueprint
     from bikespace_api.api.docs import docs_blueprint
+    from bikespace_api.api.views import AdminModelView
+    from bikespace_api.api.models import Submission, User, Role
+
+    global user_datastore
+    #user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+    user_datastore = create_userdatastore(db, User, Role)
+    security = Security(app, user_datastore)
 
     app.register_blueprint(submissions_blueprint, url_prefix="/api/v2")
     app.register_blueprint(docs_blueprint, url_prefix="/api/v2")
+
+    admin = Admin(app, name='BikeSpace', template_mode='bootstrap3', base_template="my_master.html")
+    admin.add_view(AdminModelView(Role, db.session))
+    admin.add_view(AdminModelView(User, db.session))
+    admin.add_view(AdminModelView(Submission, db.session))
+
+    @security.context_processor
+    def security_context_processor():
+        return dict(
+            admin_base_template=admin.base_template,
+            admin_view=admin.index_view,
+            h=admin_helpers,
+            get_url=url_for
+        )
+
+    @app.route("/admin/")
+    def admin_index():
+        return render_template("admin/index.html")
 
     @app.route("/")
     def api_home_page():
---
+++ bikespace_api/bikespace_api/api/views.py
@@ -0,0 +1,47 @@
+from flask import abort, redirect, request, url_for
+from flask_admin.contrib.sqla import ModelView 
+from flask_security import current_user
+from wtforms import SelectField
+from bikespace_api.api.models import IssueType, ParkingDuration
+
+class AdminModelView(ModelView):
+    def is_accessible(self):
+        return (
+            current_user.is_active
+            and current_user.is_authenticated
+            and current_user.has_role("superuser")
+        )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not
+        accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for("security.login", next=request.url))
+
+class AdminSubmissionModelView(ModelView):
+    def is_accessible(self):
+        return (
+            current_user.is_active
+            and current_user.is_authenticated
+            and current_user.has_role("superuser")
+        )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not
+        accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for("security.login", next=request.url)) 
