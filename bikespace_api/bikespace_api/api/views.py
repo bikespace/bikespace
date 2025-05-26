6cd24062b21fb78e8118f5dded932e11daeeb674
Sonal Ranjit <sonal.ranjit3@gmail.com>
2025-05-26 07:58:49 -0400

    Separate modelviews according to models

--- bikespace_api/bikespace_api/api/views.py
+++ bikespace_api/bikespace_api/api/views.py
@@ -3,8 +3,35 @@ from flask_admin.contrib.sqla import ModelView
 from flask_security import current_user
 from wtforms import SelectField
 from bikespace_api.api.models import IssueType, ParkingDuration
+import uuid
 
-class AdminModelView(ModelView):
+
+class AdminRolesModelView(ModelView):
+    def is_accessible(self):
+        return (
+            current_user.is_active
+            and current_user.is_authenticated
+            and current_user.has_role("superuser")
+        )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not
+        accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for("security.login", next=request.url)) 
+
+class AdminUsersModelView(ModelView):
+    column_hide_backrefs = False
+    column_list = ('first_name', 'last_name', 'email', 'active', 'roles')
+    column_labels = {'first_name': 'First Name','last_name': 'Last Name','email': 'Email', 'active': 'Active', 'roles': 'Roles'}
+    form_excluded_columns = ('fs_uniquifier')
     def is_accessible(self):
         return (
             current_user.is_active
@@ -24,6 +51,10 @@ class AdminModelView(ModelView):
             else:
                 # login
                 return redirect(url_for("security.login", next=request.url))
+
+    def on_model_change(self, form, model, is_created):
+        if model.fs_uniquifier is None:
+            model.fs_uniquifier = uuid.uuid4().hex
 
 class AdminSubmissionModelView(ModelView):
     def is_accessible(self):
