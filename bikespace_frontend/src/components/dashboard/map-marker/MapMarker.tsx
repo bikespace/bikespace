59e80cdeaf72517f545d594eae7e31ce470eefbf
Ben Coleman <ben.coleman@alum.utoronto.ca>
2025-06-07 17:34:50 -0400

    Revert "WIP simplify map handling"
    This reverts commit b5275048d24c6fabe72aa6a2fc2ea19b2821d6ae.

--- bikespace_frontend/src/components/dashboard/map-marker/MapMarker.tsx
+++ bikespace_frontend/src/components/dashboard/map-marker/MapMarker.tsx
@@ -39,11 +39,14 @@ import {wrapperFullWidth} from '@/styles/variablesTS';
 
 interface MapMarkerProps {
   submission: SubmissionApiPayload;
+  windowWidth: number | null;
+  doneLoading: boolean;
+  clusterRef: MutableRefObject<LeafletMarkerClusterGroup | null>;
 }
 
 const MapMarker = forwardRef(
   (
-    {submission}: MapMarkerProps,
+    {submission, windowWidth, doneLoading, clusterRef}: MapMarkerProps,
     outerMarkerRef: React.ForwardedRef<LeafletMarker>
   ) => {
     // popupRef for calling openPopup() upon focus change
@@ -69,8 +72,20 @@ const MapMarker = forwardRef(
     const iconHeight = isFocused ? baseIconHeight * 1.5 : baseIconHeight;
     const iconWidth = iconHeight;
 
+    // focus pin on load if in URL param
+    useEffect(() => {
+      if (!isFocused || !doneLoading) return;
+      if (windowWidth && windowWidth <= wrapperFullWidth) {
+        setTab(SidebarTab.Feed);
+        setIsOpen(true);
+      }
+      clusterRef.current!.zoomToShowLayer(innerMarkerRef.current!, () => {
+        innerMarkerRef.current!.openPopup();
+      });
+    }, [doneLoading]);
+
     const handlePopupClose = () => {
-      if (isFocused) setFocus(null);
+      if (focus === submission.id) setFocus(null);
     };
 
     const handlePopupOpen = () => {
@@ -80,7 +95,17 @@ const MapMarker = forwardRef(
     };
 
     const handleClick = () => {
-      setFocus(submission.id);
+      if (windowWidth && windowWidth <= wrapperFullWidth) {
+        // Manually set tab= URL params to prevent excess rerendering from subscribing to tab change
+        const params = new URLSearchParams(searchParams);
+
+        params.set('tab', SidebarTab.Feed);
+
+        replace(`${pathname}?${params.toString()}` as Route);
+        setFocus(submission.id);
+        setTab(SidebarTab.Feed);
+        setIsOpen(true);
+      }
     };
 
     const priorityIssue = submission.issues.reduce((a: IssueType | null, c) => {
