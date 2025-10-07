4e7bacdfaa08c5eb7029f1e84953354d23b0666f
Sonal Ranjit <sonal.ranjit3@gmail.com>
2025-10-07 18:19:04 -0400

    Add more functional tests for admin page

--- bikespace_api/bikespace_api/tests/functional/test_admin_page.py
+++ bikespace_api/bikespace_api/tests/functional/test_admin_page.py
@@ -60,3 +60,24 @@ def default_login_page_redirect(response: str, soup: BeautifulSoup):
     assert soup.find('a', class_="navbar-brand", href="/admin", string="BikeSpace")
     assert soup.find('input', id="submit", class_="btn btn-primary", type="submit", value="Login")
     assert soup.find('a', href="/admin/", string="Home")
+
+def test_admin_login_successfully(test_client):
+    
+    """
+    GIVEN the flask application configured for testing
+    WHEN the '/admin/login' page is posted to (POST) with valid credentials
+    THEN check that the response is a redirection to the admin home page
+    """
+
+    response = test_client.post(
+        "/admin/login",
+        data=dict(email="admin@example.com", password="admin"),
+        follow_redirects=True)
+    soup = BeautifulSoup(response.data, 'html.parser')
+
+    assert response.status_code == 200
+    assert soup.find('a', class_="navbar-brand", href="/admin", string="BikeSpace")
+    assert soup.find('a', href="/admin/", string="Home")
+    assert not soup.find('a', class_="btn btn-primary", href="/admin/login", string="login")
+    assert soup.find('a', href="/admin/", string="Home")
+
