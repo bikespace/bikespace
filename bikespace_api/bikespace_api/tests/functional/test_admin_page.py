5562ebb82a856cfa6e8427fd59a5d5e8681b076b
Sonal Ranjit <sonal.ranjit3@gmail.com>
2025-10-06 20:26:06 -0400

    Add more functional test

--- bikespace_api/bikespace_api/tests/functional/test_admin_page.py
+++ bikespace_api/bikespace_api/tests/functional/test_admin_page.py
@@ -1,4 +1,5 @@
 from bikespace_api.api.models import Role, User
+from bs4 import BeautifulSoup
 
 def test_admin_page(test_client):
 
@@ -8,8 +9,54 @@ def test_admin_page(test_client):
     THEN check that the response is valid
     """
     response = test_client.get("/admin/")
+    soup = BeautifulSoup(response.data, 'html.parser')
     assert response.status_code == 200
-    assert b"<a class=\"navbar-brand\" href=\"/admin\">BikeSpace</a>" in response.data
-    assert b"<a class=\"btn btn-primary\" href=\"/admin/login\">login</a>" in response.data
-    assert b"<a href=\"/admin/\">Home</a>" in response.data
-    assert b"<a class=\"btn btn-primary\" href=\"/\">" in response.data
+    assert soup.find('h1', string="BikeSpace Admin")
+    assert soup.find('a', class_="navbar-brand", href="/admin", string="BikeSpace")
+    assert soup.find('a', class_="btn btn-primary", href="/admin/login", string="login")
+    assert soup.find('a', href="/admin/", string="Home")
+
+def test_admin_page_submision_without_logging_in(test_client):
+
+    """
+    GIVEN the flask application configured for testing
+    WHEN the '/admin/submission/' page is requested (GET) without logging in
+    THEN check that the response is a redirection to the login page
+    """
+    response = test_client.get("/admin/submission/", follow_redirects=True)
+    soup = BeautifulSoup(response.data, 'html.parser')
+
+    default_login_page_redirect(response, soup)
+
+def test_admin_page_roles_without_logging_in(test_client):
+
+    """
+    GIVEN the flask application configured for testing
+    WHEN the '/admin/roles/' page is requested (GET) without logging in
+    THEN check that the response is a redirection to the login page
+    """
+
+    response = test_client.get("/admin/role/", follow_redirects=True)
+    soup = BeautifulSoup(response.data, 'html.parser')
+
+    default_login_page_redirect(response, soup)
+
+
+def test_admin_page_user_without_logging_in(test_client):
+
+    """
+    GIVEN the flask application configured for testing
+    WHEN the '/admin/user/' page is requested (GET) without logging in
+    THEN check that the response is a redirection to the login page
+    """
+    response = test_client.get("/admin/user/", follow_redirects=True)
+    soup = BeautifulSoup(response.data, 'html.parser')
+
+    default_login_page_redirect(response, soup)
+
+def default_login_page_redirect(response: str, soup: BeautifulSoup):
+    assert response.status_code == 200 
+    assert soup.find('h1', string="Login")
+    assert soup.find('a', class_="navbar-brand", href="/admin", string="BikeSpace")
+    assert soup.find('input', id="submit", class_="btn btn-primary", type="submit", value="Login")
+    assert soup.find('a', href="/admin/", string="Home")
