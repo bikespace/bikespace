682bd9088a5eb60ed958fb5a1ee6967e98ed3d9e
Ben Coleman <ben.coleman@alum.utoronto.ca>
2025-12-20 15:09:27 -0500

    Add basemap switching
    - added simple UI element to change basemap to parking map page
    - changed Bicycle Parking and Bicycle Network layers so that their first layer id is a specified property instead of imported
    - added basemap option for regular City of Toronto Aerial imagery
    - added attribution property for layers that use City data

--- bikespace_frontend/src/components/map-layers/basemaps/BasemapControl.tsx
+++ bikespace_frontend/src/components/map-layers/basemaps/BasemapControl.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
 
-import {Layer, Source} from 'react-map-gl/maplibre';
-
 import {CustomControlOverlay} from '@/utils/map-utils';
 
+export interface BaseMapOption {
+  id: string;
+  name: string;
+  component: React.JSX.Element | null;
+}
+
 interface BasemapControlProps {
-  selectedLayer: 'cot-aerial' | 'cot-aerial-night' | 'maptiler-streets';
+  layerOptions: BaseMapOption[];
+  selectedLayer: string | undefined;
   setSelectedLayer: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export function BasemapControl({
+  layerOptions,
   selectedLayer,
   setSelectedLayer,
 }: BasemapControlProps) {
   return (
     <CustomControlOverlay position="bottom-left">
-      <div style={{padding: 4}}>Hello World</div>
+      <div style={{padding: 4}}>
+        <fieldset>
+          <legend>Basemap:</legend>
+          {layerOptions.map(layer => (
+            <div key={layer.id}>
+              <input
+                type="radio"
+                id={layer.id}
+                name={layer.name}
+                checked={selectedLayer?.includes(layer.id)}
+                onChange={e => {
+                  if (e.target.checked) {
+                    setSelectedLayer(layer.id);
+                  } else {
+                    setSelectedLayer(layerOptions[0].id);
+                  }
+                }}
+              />
+              <label htmlFor={layer.id}>{layer.name}</label>
+            </div>
+          ))}
+        </fieldset>
+      </div>
     </CustomControlOverlay>
   );
 }
--- bikespace_frontend/src/components/map-layers/basemaps/index.ts
+++ bikespace_frontend/src/components/map-layers/basemaps/index.ts
@@ -1,1 +1,2 @@
 export * from './BasemapControl';
+export * from './BaseMapOptions';
