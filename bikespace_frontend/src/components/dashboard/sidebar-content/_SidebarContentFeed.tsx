9b329358d5580ead88f8b4fc88fb1b8346fe6194
Ben Coleman <ben.coleman@alum.utoronto.ca>
2026-03-19 05:44:38 -0400

    Simplify FeedSubmissionItem
    move handling up one level

--- bikespace_frontend/src/components/dashboard/sidebar-content/_SidebarContentFeed.tsx
+++ bikespace_frontend/src/components/dashboard/sidebar-content/_SidebarContentFeed.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 import {useStore} from '@/states/store';
 import {useSubmissionsQuery} from '@/hooks';
+import {useIsMobile} from '@/hooks/use-is-mobile';
+import {useSubmissionId} from '@/states/url-params';
+
+import {trackUmamiEvent} from '@/utils';
 
 import {Spinner} from '@/components/shared-ui/spinner';
 
@@ -9,9 +13,25 @@ import {FeedSubmissionItem} from '../feed-submission-item';
 
 import styles from './_SidebarContent.module.scss';
 
+type ItemRef = Record<number, HTMLButtonElement>;
+
 export function SidebarContentFeed() {
   const submissions = useStore(state => state.submissions);
+  const [focusedSubmissionId, setFocusedSubmissionId] = useSubmissionId();
   const {isLoading} = useSubmissionsQuery();
+  const isMobile = useIsMobile();
+
+  const itemRefs = useRef<ItemRef>({});
+
+  // scroll selected item into view when:
+  // - focus changes
+  // - submissions change (e.g. more are loaded)
+  // - viewport changes from desktop to mobile or vice versa
+  useEffect(() => {
+    if (!focusedSubmissionId) return;
+
+    itemRefs.current[focusedSubmissionId]?.scrollIntoView();
+  }, [focusedSubmissionId, submissions, isMobile]);
 
   return (
     <>
@@ -23,7 +43,20 @@ export function SidebarContentFeed() {
         data-testid="submissions-feed"
       >
         {[...submissions].reverse().map(submission => (
-          <FeedSubmissionItem key={submission.id} submission={submission} />
+          <FeedSubmissionItem
+            key={submission.id}
+            submission={submission}
+            isFocused={submission.id === focusedSubmissionId}
+            onClick={() => {
+              setFocusedSubmissionId(submission.id);
+              trackUmamiEvent('focus_submission', {
+                submission_id: submission.id,
+              });
+            }}
+            ref={(element: HTMLButtonElement) => {
+              itemRefs.current[submission.id] = element;
+            }}
+          />
         ))}
         <div className={styles.loadingIndicator}>
           {isLoading ? <Spinner label="Loading submissions..." /> : null}
